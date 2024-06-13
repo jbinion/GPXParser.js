bfcaebbe938e665e336e6f04dcf26d4a4bc2c03e
jbinion <jbinion0366@gmail.com>
2024-06-13 16:46:16 -0400

    Update formatTrk.js

--- src/formatTrk.js
+++ src/formatTrk.js
@@ -1,23 +1,22 @@
 const formatTrk = (trk) => {
+  console.log(trk);
   const name = trk.children[0].name.content;
   const color =
     trk.children[1]?.extensions?.children[0]["gpxx:TrackExtension"].children[0][
       "gpxx:DisplayColor"
     ].content || "orange";
   const waypoints =
-    trk.children[2]?.trkseg.children.map((t) => formatTrkpt(t)) || [];
+    trk.children[2]?.trkseg.children.map((t) => formatTrkpt(t.trkpt)) || [];
   return { name, color, waypoints };
 };
 
 /**
- * @param trkptObj an object with one field -- trkpt
- * @param trkptObj.trkpt the track point
- * @param trkptObj.trkpt.lat
- * @param trkptObj.trkpt.long
- * @param trakObj.trkpt.children
+ * @param {Object} trkpt
+ * @param {string} trkpt.lat
+ * @param {string} trkpt.long
  */
-const formatTrkpt = (trkptObj) => {
-  const { lat, lon } = trkptObj.trkpt;
+const formatTrkpt = (trkpt) => {
+  const { lat, lon } = trkpt;
   return { latitude: Number(lat), longitude: Number(lon) };
 };
 module.exports = formatTrk;
