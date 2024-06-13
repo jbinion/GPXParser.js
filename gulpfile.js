a6c6eade855955cbe991b481bb862b919e9742a5
jbinion <jbinion0366@gmail.com>
2024-06-13 16:46:27 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,40 @@
-var gulp = require('gulp'),
-    minify = require('gulp-minify'),
-    jsdoc = require('gulp-jsdoc3'),
-    mocha = require('gulp-mocha');
-
-gulp.task('test', function() {
-    return gulp.src('./test/test.js', {read: false}).pipe(mocha({reporter: 'nyan'}))
+var gulp = require("gulp"),
+  minify = require("gulp-minify"),
+  jsdoc = require("gulp-jsdoc3"),
+  mocha = require("gulp-mocha");
+var jest = require("gulp-jest").default;
+gulp.task("test", function () {
+  return gulp.src("./test/parse.test.js", { read: false }).pipe(
+    jest({
+      preprocessorIgnorePatterns: [
+        "<rootDir>/dist/",
+        "<rootDir>/node_modules/",
+      ],
+      automock: false,
+    })
+  );
 });
 
-gulp.task('minify', function(){
-    return gulp.src('./src/GPXParser.js')
-    .pipe(minify({
-        ext:{
-            min:'.min.js'
+gulp.task("minify", function () {
+  return gulp
+    .src("./src/index.js")
+    .pipe(
+      minify({
+        ext: {
+          min: ".min.js",
         },
-        noSource: true
-      }))
-    .pipe(gulp.dest('./dist'));
+        noSource: true,
+      })
+    )
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('doc', function (cb) {
-    var config = require('./jsdoc.json');
+gulp.task("doc", function (cb) {
+  var config = require("./jsdoc.json");
 
-    return gulp.src(['README.md', './src/GPXParser.js'], {read: false}).pipe(jsdoc(config, cb));
+  return gulp
+    .src(["README.md", "./src/index.js"], { read: false })
+    .pipe(jsdoc(config, cb));
 });
 
-gulp.task('build', gulp.series('test', 'minify', 'doc'));
+gulp.task("build", gulp.series("test", "minify", "doc"));
